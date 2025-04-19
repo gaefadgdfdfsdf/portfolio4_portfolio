abd8d010b900dddca75a3cf3bb78ae0bc87d1be7
Jo yun ha <ehdhbd688@naver.com>
2025-04-19 14:30:57 +0900

    g

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './reset.css';
 import Section01 from './section/section01';
+import Section02 from './section/section02';
 
 function App() {
   return (
     <>
    
       <Section01/>
+      <Section02/>
       
     </>
   );
--- src/section/section01.js
+++ src/section/section01.js
@@ -162,48 +162,7 @@ const Section01 = () => {
             </div>
         </div>
      </div>
-     <div className="mb-5 lg:mb-[3.6111111111111107vw]">
-        <div className="max-w-full pl-[1.38vw] pr-[1.38vw]">
-        <div className="pr-[60px] lg:pr-[11.527777777777779vw] relative">
-				<div class="absolute right-0 lg:right-[1.3194444444444444vw] -top-0.5 lg:top-[-0.06944444444444445vw] text-[18px] lg:text-[2.2222222222222223vw] leading-[100%]">
-					<span class="inline-flex mx-[3px] lg:mx-[0.4166666666666667vw]">
-						 ( 03 )					</span>
-				</div>
-				<div>
-                 <img className='w-full h-auto max-w-full' src={process.env.PUBLIC_URL + 'featured.svg'} alt='portfolio'/>
-				</div>
-			</div>
-        </div>
-     </div>  
-     <div className='hidden h-[7.916666666666666vw]'></div> 
-     <section className='pb-[10.416666666666668vw]'>
-        <div className='w-full bg-black h-[0.5px]'></div>
-        <div className='max-w-full pl-[1.38vw]  pr-[1.38vw] '>
-            <div className='relative'>
-                <div className='lg:sticky lg:top-[var(--header-sticky-height)] lg:grid'>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'>
-                        <div className='absolute bottom-0 left-0 w-full bg-black h-[0.5px]'></div>
-                        <a href='' className='absolute inset-0 z-[1]'/>
-                        <div className='py-5 lg:py-[1.38vw] flex flex-col-reverse lg:flex-row lg:h-full'>
-                            <div className='lg:w-[32.84722222222222vw] lg:pr-[2.7777777777777777vw] mt-[18px] lg:mt-0'>
-                                <h2 className='text-[32px] lg:text-[4.166666666666666vw] leading-[100%] tracking-tighter font-medium'>hyundai</h2>
-                                <div className='text-[14px] lg:text-[0.9722222222222222vw] mt-2.5 lg:mt-[1vw] font-medium'>html & css & JavaScript</div>
-                                <div className='text-[20px] description lg_br_enable mt-5 lg:mt-[2vw] lg:tracking-tight leading-[134%]'>
-                                이 포트폴리오 사이트는 HTML, CSS, JavaScript로 제작하였고, Swiper.js를 이용해 슬라이드 기능을 구현했습니다.
-                                GSAP 라이브러리를 활용해 텍스트나 이미지에 자연스러운 애니메이션 효과를 더해 생동감을 주었습니다.
-                                슬라이드가 넘어갈 때마다 요소들이 부드럽게 등장하거나 사라지도록 처리해 사용자 몰입도를 높였습니다.
-                                전체적으로 구조가 깔끔하고, 인터랙션이 잘 적용되어 보기 편한 웹사이트입니다.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'></div>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'></div>
-                    <div></div>
-                </div>
-            </div>
-        </div>
-     </section>
+    
     </>
   )
 }

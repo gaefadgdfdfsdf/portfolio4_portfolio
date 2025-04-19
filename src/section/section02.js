9b3ee72b5cf443045328bd51c1bda598bb6504af
Jo yun ha <ehdhbd688@naver.com>
2025-04-19 16:50:46 +0900

    f

--- src/section/section02.js
+++ src/section/section02.js
@@ -22,8 +22,8 @@ const Section02 = () => {
         <div className='max-w-full pl-[1.38vw]  pr-[1.38vw] '>
             <div className='relative'>
                 <div className='lg:sticky lg:top-[var(--header-sticky-height)] lg:grid'>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'>
-                        <div className='absolute bottom-0 left-0 w-full bg-black h-[0.5px]'></div>
+                    <div className='lg:h-[34.29166666666667vw] overflow-hidden relative'>
+                        <div className='absolute bottom-0 left-0 w-full bg-black h-[1px] z-10'></div>
                         <a href='https://gaefadgdfdfsdf.github.io/portfolio_1_hyundai/' target='_blank' className='absolute inset-0 z-[1]'/>
                         <div className='py-5 lg:py-[1.38vw] flex flex-col-reverse lg:flex-row lg:h-full'>
                             <div className='lg:w-[32.84722222222222vw] lg:pr-[2.7777777777777777vw] mt-[18px] lg:mt-0'>
@@ -46,8 +46,54 @@ const Section02 = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'></div>
-                    <div className='lg:h-[32.29166666666667vw] overflow-hidden relative'></div>
+                    <div className='lg:h-[34.29166666666667vw] overflow-hidden relative'>
+                        <div className='absolute bottom-0 left-0 w-full bg-black h-[1px] z-10'></div>
+                        <a href='https://gaefadgdfdfsdf.github.io/portfolio2_glaxywatch/' target='_blank' className='absolute inset-0 z-[1]'/>
+                        <div className='py-5 lg:py-[1.38vw] flex flex-col-reverse lg:flex-row lg:h-full'>
+                            <div className='lg:w-[32.84722222222222vw] lg:pr-[2.7777777777777777vw] mt-[18px] lg:mt-0'>
+                                <h2 className='text-[32px] lg:text-[4.166666666666666vw] leading-[100%] tracking-tighter font-medium'>glaxywatch</h2>
+                                <div className='text-[14px] lg:text-[0.9722222222222222vw] mt-2.5 lg:mt-[1vw] font-medium'>html & css & JavaScript</div>
+                                <div className='text-[16px] description lg_br_enable mt-5 lg:mt-[2vw] lg:tracking-tight leading-[134%]'>
+                                이 포트폴리오 사이트는 HTML, CSS, JavaScript로 제작하였고, Swiper.js를 이용해 슬라이드 기능을 구현했습니다.
+                                GSAP 라이브러리를 활용해 텍스트나 이미지에 자연스러운 애니메이션 효과를 더해 생동감을 주었습니다.
+                                슬라이드가 넘어갈 때마다 요소들이 부드럽게 등장하거나 사라지도록 처리해 사용자 몰입도를 높였습니다.
+                                전체적으로 구조가 깔끔하고, 인터랙션이 잘 적용되어 보기 편한 웹사이트입니다.
+                                </div>
+                            </div>
+                            <div className='lg:w-[calc(100%-32.84722222222222vw)]'>
+                                <div className='wrap-img-distortion overflow-hidden rounded-[.3472222222222222vw] h-[42vw] lg:h-full relative'>
+                                    <img className='object-cover w-full relative h-full lg:top-1/2 lg:-translate-y-1/2' src={process.env.PUBLIC_URL + 'galaxy.jpg'} alt='galaxy'/>
+                                    <div class="absolute top-[50%] w-full left-0 z-1 translate-y-[-50%]">
+                                        <canvas className='w-full h-full object-cover' id="hover-effect-canvas-1745038706574" width="1236" height="618"></canvas>
+                                    </div>
+				                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='lg:h-[34.29166666666667vw] overflow-hidden relative'>
+                    <div className='absolute bottom-0 left-0 w-full bg-black h-[1px] z-10'></div>
+                        <a href='https://gaefadgdfdfsdf.github.io/portfolio2_glaxywatch/' target='_blank' className='absolute inset-0 z-[1]'/>
+                        <div className='py-5 lg:py-[1.38vw] flex flex-col-reverse lg:flex-row lg:h-full'>
+                            <div className='lg:w-[32.84722222222222vw] lg:pr-[2.7777777777777777vw] mt-[18px] lg:mt-0'>
+                                <h2 className='text-[32px] lg:text-[4.166666666666666vw] leading-[100%] tracking-tighter font-medium'>herryporter</h2>
+                                <div className='text-[14px] lg:text-[0.9722222222222222vw] mt-2.5 lg:mt-[1vw] font-medium'>html & css & JavaScript</div>
+                                <div className='text-[16px] description lg_br_enable mt-5 lg:mt-[2vw] lg:tracking-tight leading-[134%]'>
+                                이 포트폴리오 사이트는 HTML, CSS, JavaScript로 제작하였고, Swiper.js를 이용해 슬라이드 기능을 구현했습니다.
+                                GSAP 라이브러리를 활용해 텍스트나 이미지에 자연스러운 애니메이션 효과를 더해 생동감을 주었습니다.
+                                슬라이드가 넘어갈 때마다 요소들이 부드럽게 등장하거나 사라지도록 처리해 사용자 몰입도를 높였습니다.
+                                전체적으로 구조가 깔끔하고, 인터랙션이 잘 적용되어 보기 편한 웹사이트입니다.
+                                </div>
+                            </div>
+                            <div className='lg:w-[calc(100%-32.84722222222222vw)]'>
+                                <div className='wrap-img-distortion overflow-hidden rounded-[.3472222222222222vw] h-[42vw] lg:h-full relative'>
+                                    <img className='object-cover w-full relative h-full lg:top-1/2 lg:-translate-y-1/2' src={process.env.PUBLIC_URL + 'herryporter-1.jpg'} alt='herryporter'/>
+                                    <div class="absolute top-[50%] w-full left-0 z-1 translate-y-[-50%]">
+                                        <canvas className='w-full h-full object-cover' id="hover-effect-canvas-1745038706574" width="1236" height="618"></canvas>
+                                    </div>
+				                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div></div>
                 </div>
             </div>

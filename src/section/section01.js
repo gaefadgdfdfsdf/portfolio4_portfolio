db5569e7f5a9c3f2be5597ee0fb0dec05a17b7b7
Jo Yun Ha <ehdhbd68@naver.com>
2025-04-29 18:55:46 +0900

    D

--- src/section/section01.js
+++ src/section/section01.js
@@ -179,12 +179,12 @@ const Section01 = () => {
                         </li>
                         <li>
                             <a href="https://atolldigital.com/about/" className="overflow-hidden leading-none h-[3.472vw]">
-                                <span className="relative flex flex-col overflow-hidden">
-                                    <span className="!opacity-100 flex nowrap items-center absolute pl-[1.041555vw]" >
+                                <span className="group relative flex flex-col overflow-hidden">
+                                    <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
                                         ABOUT
                                     </span>
-                                    <span className="!opacity-100 flex nowrap items-center pl-[1.041555vw]">
+                                    <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-center">
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
                                         ABOUT
                                     </span>
@@ -193,12 +193,12 @@ const Section01 = () => {
                         </li>
                         <li className="flex items-center">
                             <a href="https://atolldigital.com/work/" className="overflow-hidden leading-none flex h-[3.472vw] items-center">
-                                <span className="relative flex flex-col overflow-hidden ">
-                                    <span className="!opacity-100 flex nowrap items-center absolute pl-[1.041555vw]" >
+                                <span className="group relative flex flex-col overflow-hidden ">
+                                    <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
                                         WORK
                                     </span>
-                                    <span className="!opacity-100 flex nowrap items-center pl-[1.041555vw]">
+                                    <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-cente">
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
                                         WORK
                                     </span>
@@ -210,12 +210,12 @@ const Section01 = () => {
                         </li>
                         <li>
                             <a href="https://atolldigital.com/contact/" className="overflow-hidden leading-none contact  h-[3.472vw]">
-                                <span className="relative flex flex-col overflow-hidden">
-                                    <span className="!opacity-100 flex nowrap items-center absolute pl-[1.441555vw]" >
+                                <span className="group relative flex flex-col overflow-hidden">
+                                    <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[1.194vw] h-[0.994vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'thesun_33.svg'} alt='thesun_33' />
                                         CONTACT
                                     </span>
-                                    <span className="!opacity-100 flex nowrap items-center pl-[1.441555vw]">
+                                    <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-cente">
                                         <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[1.194vw] h-[0.994vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'thesun_33.svg'} alt='thesun_33' />
                                         CONTACT
                                     </span>
@@ -229,11 +229,12 @@ const Section01 = () => {
                 <div className="transition-colors duration-300 max-lg:hidden cursor-pointer absolute right-0 inset-y-0 flex items-center justify-center">
                     <div className="block bg-black absolute top-0 left-0 w-[0.5px] h-full"></div>
                     <a href="https://atolldigital.com/fr/" className="h-[3.472vw] text-[1vw] font-medium px-[1vw] flex items-center justify-center w-full overflow-hidden leading-none">
-                        <span class="relative flex flex-col overflow-hidden">
-                            <span className="!opacity-100 absoloute flex nowrap items-center absolute top-0 left-0">FR</span>
-                            <span className="!opacity-100 flex nowrap items-center  ">FR</span>
+                        <span class="group relative flex flex-col overflow-hidden">
+                            <span className="translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center">GIT</span>
+                            <span className="translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-center">GIT</span>
                         </span>
                     </a>
+                
                 </div>
                 <span className="max-lg:hidden block border-b border-current absolute bottom-0 left-0 w-full"></span>
             </div>
@@ -263,41 +264,45 @@ const Section01 = () => {
                         <div className='max-lg:hidden px-[var(--size-20)] flex justify-between'>
                             <ul className="menu_header lowercase flex items-center gap-[2.083333333333333vw] font-medium text-[20px]">
                                 <li>
-                                    <a className='pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center overflow-hidden'>
-                                        <span className="relative flex flex-col overflow-hidden">
-                                            <span id="about" className="opacity-0 left-[1.04vw] absolute flex flex-nowrap items-center">
-                                                About
+                                    <a href="#" className='pl-[1.388vw] pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0 relative flex items-center overflow-hidden'>
+                                        <span className="group relative flex flex-col overflow-hidden">
+                                            <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
+                                                <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
+                                                ABOUT
                                             </span>
-                                            <span className="flex flex-nowrap items-center opacity-1 pl-[1.04vw]">
-                                                <svg className="mt-[4px] mr-[5px]" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none"><path d="M10 0L10 10L0 1.90735e-06L10 0Z" fill="gray" /></svg>
-                                                about
+                                            <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-center">
+                                                <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
+                                                ABOUT
                                             </span>
                                         </span>
                                     </a>
+
                                 </li>
                                 <li className="flex items-center">
-                                    <a className="pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center">
-                                        <span className="relative flex flex-col overflow-hidden">
-                                            <span id="about" className="opacity-0 left-[1.04vw] absolute flex flex-nowrap items-center">
-                                                work
+                                    <a href="#" className="pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center">
+                                        <span className="group relative flex flex-col overflow-hidden">
+                                            <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
+                                            <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
+                                            WORK
                                             </span>
-                                            <span className="flex flex-nowrap items-center opacity-1 pl-[1.04vw]">
-                                                <svg className="mt-[4px] mr-[5px]" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none"><path d="M10 0L10 10L0 1.90735e-06L10 0Z" fill="gray" /></svg>
-                                                work
+                                            <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-cente">
+                                                <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[0.694vw] h-[0.694vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'trai_icon.svg'} alt='trai_icon' />
+                                                WORK
                                             </span>
                                         </span>
                                     </a>
                                     <span className="relative text-[12px] -top-[5px] left-[3px]">(10)</span>
                                 </li>
                                 <li>
-                                    <a className='pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center overflow-hidden'>
-                                        <span className="relative flex flex-col overflow-hidden">
-                                            <span id="about" className="opacity-0 left-[1.04vw] absolute flex flex-nowrap items-center">
-                                                contact
+                                    <a href="#" className='pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center overflow-hidden'>
+                                        <span className="group relative flex flex-col overflow-hidden">
+                                            <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
+                                            <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[1.194vw] h-[0.994vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'thesun_33.svg'} alt='thesun_33' />
+                                            CONTACT
                                             </span>
-                                            <span className="flex flex-nowrap items-center opacity-1 pl-[1.04vw]">
-                                                <svg className="mt-[4px] mr-[5px]" xmlns="http://www.w3.org/2000/svg" width="20" height="13" viewBox="0 0 17 10" fill="none"><path d="M7.80992 0.461061C7.81054 0.407311 7.81116 0.353614 7.81179 0.299864C7.81189 0.282623 7.81257 0.263772 7.82451 0.250944C7.83755 0.237027 7.85905 0.235573 7.8779 0.235677C8.42225 0.23282 8.96655 0.240714 9.51017 0.259358C9.49345 0.259254 9.48991 1.18572 9.48555 2.31539C9.47927 3.94688 9.47137 6.00224 9.41981 6.30246C9.70398 6.01076 9.98799 5.71916 10.272 5.42762C10.5678 5.12397 10.8635 4.82032 11.1594 4.51658C11.3866 4.28335 11.6137 4.05007 11.844 3.81955C12.4397 3.22311 13.0551 2.64625 13.6886 2.08996C14.0887 2.46324 14.4676 2.85953 14.8226 3.27561C14.1426 3.94039 13.4625 4.60517 12.7819 5.2699C12.6502 5.39869 12.5184 5.52738 12.3867 5.65601C12.1246 5.91219 11.8624 6.16827 11.6006 6.42455C11.5183 6.50499 11.4368 6.58751 11.355 6.67034C11.0799 6.94891 10.8012 7.231 10.4799 7.4487C12.5047 7.42414 14.5298 7.43031 16.5542 7.46667C16.5946 7.66043 16.5802 7.86083 16.5658 8.05848C16.552 8.24403 16.5384 8.42964 16.5248 8.61519C16.5111 8.80074 16.4974 8.9863 16.4837 9.1719C16.4805 9.21703 14.4031 9.19678 13.7101 9.19003C13.6095 9.18904 13.5381 9.18831 13.5064 9.18816C13.1999 9.18634 12.8931 9.18722 12.5863 9.18805C11.9971 9.18971 11.408 9.19132 10.8211 9.17413L8.58635 6.84162L6.21203 9.12038C4.33656 9.09857 2.46064 9.07775 0.584993 9.08502C0.572649 8.51309 0.586125 7.94034 0.625406 7.36935C1.4822 7.31607 2.34083 7.34125 3.19873 7.36644C3.3312 7.37028 3.46365 7.37418 3.59608 7.37781C4.15771 7.39308 4.71514 7.38436 5.27341 7.37558C5.66929 7.3694 6.06563 7.36317 6.46416 7.3655C6.42121 7.28439 6.36045 7.21438 6.29964 7.14547C5.68866 6.45581 5.00705 5.8385 4.32534 5.22108C3.65385 4.61296 2.98223 4.00468 2.37775 3.32697C2.35802 3.30475 2.33671 3.27712 2.34446 3.24804C2.34834 3.23298 2.36027 3.22067 2.37111 3.21002C2.57544 3.00567 2.77976 2.80121 2.98409 2.59671C3.18842 2.39225 3.39275 2.18779 3.59708 1.98344C3.76708 2.16448 3.93715 2.34551 4.10723 2.52655C4.44006 2.88077 4.77289 3.2351 5.10552 3.58964C5.46623 3.97394 5.82799 4.35875 6.22776 4.70135C6.28972 4.75432 6.35245 4.8063 6.41519 4.85829C6.51853 4.94392 6.62187 5.02956 6.72158 5.1195C7.07794 5.44075 7.38226 5.81492 7.68492 6.18748C7.72169 6.2322 7.7542 6.03106 7.75658 6.01637C7.75664 6.0159 7.75669 6.01554 7.75669 6.01554C7.76313 5.9581 7.75954 5.89318 7.75607 5.82936C7.75388 5.78974 7.7517 5.75048 7.75201 5.71371C7.75487 5.47628 7.75721 5.23838 7.75954 5.00094V4.99856L7.77502 3.59972C7.7853 2.66089 7.79564 1.72207 7.8054 0.783817L7.80649 0.783246C7.80732 0.675851 7.80862 0.568456 7.80992 0.461061Z" fill="gray" /></svg>
-                                                contact
+                                            <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-cente">
+                                                <img className='absolute top-1/5 -translate-y-1/3 opacity-[0.2] left-0 w-[1.194vw] h-[0.994vw] mt-[0.694vw] block' src={process.env.PUBLIC_URL + 'thesun_33.svg'} alt='thesun_33' />
+                                                CONTACT
                                             </span>
                                         </span>
                                     </a>
@@ -305,8 +310,15 @@ const Section01 = () => {
 
                             </ul>
                             <div className='flex items-center justify-center mr-6'>
-                                <a href="#" className="flex items-center justify-center">
-                                    GIT
+                                <a href="#" className='pt-[1.5277777777777777vw] pb-[1.5277777777777777vw] px-0relative flex items-center overflow-hidden'>
+                                    <span className="group relative flex flex-col overflow-hidden">
+                                        <span className="pl-[1.041555vw] translate-y-full  group-hover:translate-y-0 group-hover:opacity-100 transition-transform duration-300 absolute top-0 left-0 flex nowrap items-center" >
+                                        GIT
+                                        </span>
+                                        <span className="pl-[1.041555vw] translate-y-0 opacity-100 group-hover:-translate-y-full  transition-transform duration-300 flex nowrap items-cente">
+                                        GIT
+                                        </span>
+                                    </span>
                                 </a>
                             </div>
                         </div>

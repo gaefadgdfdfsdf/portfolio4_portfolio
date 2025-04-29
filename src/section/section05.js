411f292f6e495880f27eaea36e2338f003c0e006
Jo Yun Ha <ehdhbd68@naver.com>
2025-04-29 16:56:03 +0900

    f

--- src/section/section05.js
+++ src/section/section05.js
@@ -2,90 +2,129 @@ import React from 'react'
 
 const Section05 = () => {
   return (
-   <>
-    <section className='my-[100px] lg:my-[8.333333333333332vw] overflow-hidden'>
-      <div className='max-w-full pl-[1.388vw] pr-[1.388vw]'>
-        <div className='max-lg:max-w-[550px] max-lg:mx-auto mb-[-5px] lg:mb-0'>
-          <h2 className='text-[76px] lg:text-[9.722222222222223vw] tracking-tight font-medium leading-[100%] lg:leading-[86%]'>
-							let’s discuss your <br/>
-              next 
+    <>
+      <section className='my-[100px] lg:my-[8.333333333333332vw] overflow-hidden'>
+        <div className='max-w-full pl-[1.388vw] pr-[1.388vw]'>
+          <div className='max-lg:max-w-[550px] max-lg:mx-auto mb-[-5px] lg:mb-0'>
+            <h2 className='text-[76px] lg:text-[9.722222222222223vw] tracking-tight font-medium leading-[100%] lg:leading-[86%]'>
+              let’s discuss your <br />
+              next
               <svg class="ml-[18px] relative -top-0.5 inline-block lg:hidden" width="35" height="36" viewBox="0 0 35 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M34.7468 16.7559V5.0489H27.6836V23.3571L4.99444 0.667969L0 5.66241L22.6122 28.2746L4.28723 28.2746L4.28723 35.3378H29.6754L29.7882 35.4507L34.6198 30.6191L27.7517 23.7511L34.7468 16.7559Z" fill="#091423"></path>
-							</svg>
-          </h2>
-        </div>
-        <div className='max-lg:max-w-[550px] max-lg:mx-auto lg:ml-[40.763888888888886vw] lg:mt-[-8.402777777777779vw] text-right lg:text-left'>
-          <div className='lg:mb-[6.319444444444445vw] relative'>
-            <div className='max-lg:hidden absolute left-[-13.819444444444445vw] top-[2.430555555555556vw]'>
-              <svg class="w-[35px] lg:w-[4.513888888888888vw] h-auto" width="65" height="66" viewBox="0 0 65 66" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M49.7134 13.1963L37.1131 0.596053L29.511 8.19817L49.2162 27.9033L0.375508 27.9033L0.375509 38.6543L49.0505 38.6543L29.3274 58.3775L36.9295 65.9796L64.2548 38.6543H64.4976V28.254H49.7134V13.1963Z" fill="#091423"></path>
+                <path d="M34.7468 16.7559V5.0489H27.6836V23.3571L4.99444 0.667969L0 5.66241L22.6122 28.2746L4.28723 28.2746L4.28723 35.3378H29.6754L29.7882 35.4507L34.6198 30.6191L27.7517 23.7511L34.7468 16.7559Z" fill="#091423"></path>
               </svg>
-            </div>
-            <h2 className='text-[76px] lg:text-[9.722222222222223vw] tracking-tight font-medium leading-[100%] lg:leading-[86%] whitespace-nowrap'>
-              <span className='max-lg:block'></span>
-              <span>project</span>
             </h2>
           </div>
-          <div className='text-center lg:text-left mt-10 lg:mt-0'>
-            <div>
-              <div className='lg:text-[1.25vw] lg:leading-[133%] lg:tracking-[-0.02em]'>
-                <div>
-                &nbsp;Big ambitions?											
-                </div>
-                <div>
-                &nbsp;We match the energy.											
+          <div className='max-lg:max-w-[550px] max-lg:mx-auto lg:ml-[40.763888888888886vw] lg:mt-[-8.402777777777779vw] text-right lg:text-left'>
+            <div className='lg:mb-[6.319444444444445vw] relative'>
+              <div className='max-lg:hidden absolute left-[-13.819444444444445vw] top-[2.430555555555556vw]'>
+                <svg class="w-[35px] lg:w-[4.513888888888888vw] h-auto" width="65" height="66" viewBox="0 0 65 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M49.7134 13.1963L37.1131 0.596053L29.511 8.19817L49.2162 27.9033L0.375508 27.9033L0.375509 38.6543L49.0505 38.6543L29.3274 58.3775L36.9295 65.9796L64.2548 38.6543H64.4976V28.254H49.7134V13.1963Z" fill="#091423"></path>
+                </svg>
+              </div>
+              <h2 className='text-[76px] lg:text-[9.722222222222223vw] tracking-tight font-medium leading-[100%] lg:leading-[86%] whitespace-nowrap'>
+                <span className='max-lg:block'></span>
+                <span>project</span>
+              </h2>
+            </div>
+            <div className='text-center lg:text-left mt-10 lg:mt-0'>
+              <div>
+                <div className='lg:text-[1.25vw] lg:leading-[133%] lg:tracking-[-0.02em]'>
+                  <div>
+                    &nbsp;Big ambitions?
+                  </div>
+                  <div>
+                    &nbsp;We match the energy.
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='mt-[37px] lg:mt-[2.638888888888889vw]'>
-              <a href='#' className='rounded-[5px] lg:h-[4.166666666666666vw] lg:px-[2.7777777777777777vw] lg:text-[0.9722222222222222vw] relative inline-flex items-center justify-center bg-[#091423] min-w-[223px] lg:min-w-[15.555555555555555vw]'>
-              <span className=" text-white relative flex flex-col ">
-                <span className="absolute opacity-0 flex flex-nowrap items-center">contact us</span>
-                <span className="">contact us</span>
-              </span>
-              <span className='lg:p-[0.4166666666666667vw] absolute left-0 bottom-0'>
+              <div className='mt-[37px] lg:mt-[2.638888888888889vw]'>
+                <a href='#' className='rounded-[5px] lg:h-[4.166666666666666vw] lg:px-[2.7777777777777777vw] lg:text-[0.9722222222222222vw] relative inline-flex items-center justify-center bg-[#091423] min-w-[223px] lg:min-w-[15.555555555555555vw]'>
+                  <span className=" text-white relative flex flex-col ">
+                    <span className="absolute opacity-0 flex flex-nowrap items-center">contact us</span>
+                    <span className="">contact us</span>
+                  </span>
+                  <span className='lg:p-[0.4166666666666667vw] absolute left-0 bottom-0'>
 
-              </span>
-              </a> 
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
-        </div>       
-      </div>
-    </section>
-    <footer className='bg-[#091423] lg:text-[0.9722222222222222vw] lg:h-[calc(100vh+1px-3.472vw)]'>
-      <div className='lg:grid lg:grid-cols-2 font-medium'>
-        <div>
-          <ul className="text-white">
-            <li className="group relative overflow-hidden cursor-pointer">
-              <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
-              <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
-                Name
-              </a>
-              <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">조윤하</span>
-            </li>
+        </div>
+      </section>
+      <footer className='bg-[#091423] lg:text-[0.9722222222222222vw] lg:h-[calc(100vh+1px-3.472vw)]'>
+        <div className='lg:grid lg:grid-cols-2 font-medium relative'>
+          <div className='line'>
+            <ul className="text-white">
+              <li className="group relative overflow-hidden cursor-pointer">
+                <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
+                <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
+                  Name
+                </a>
+                <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">조윤하</span>
+              </li>
 
-            <li className="group relative overflow-hidden cursor-pointer">
-              <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
-              <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
-                Birth
-              </a>
-              <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">1998.09.20</span>
-           
-            </li>
+              <li className="group relative overflow-hidden cursor-pointer">
+                <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
+                <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
+                  Birth
+                </a>
+                <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">1998.09.20</span>
 
-            <li className="group relative overflow-hidden cursor-pointer">
-              <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
-              <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
-                Contact
-              </a>
-              <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">010-8864-4851</span>
-            </li>
-          </ul>
-        </div> 
-        <div className='w-full bg-white h-[0.5px]'></div>
-      </div>
-    </footer>
-   </>
+              </li>
+
+              <li className="group relative overflow-hidden cursor-pointer">
+                <div className="group-hover:translate-y-0 hover-bg h-full bg-[#E6E8EA] absolute inset-0 translate-y-[100px] transition-transform duration-300"></div>
+                <a href="#" className="flex items-center pl-[1.388vw] pr-[1.388vw] lg:h-[3vw] lg:pt-0 lg:pb-0 lg:text-[0.9722222222222222vw] relative z-10 transition-colors duration-300 group-hover:text-black">
+                  Contact
+                </a>
+                <span className="absolute right-[1.388vw] top-1/2 -translate-y-1/2 transition-colors duration-300 group-hover:text-black">010-8864-4851</span>
+              </li>
+            </ul>
+          </div>
+          <div className="hidden lg:block absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-[0.5px] bg-[#E6E8EA]  h-full z-10"></div>
+          <div className="line2 text-white flex justify-between pr-[1.388vw] pl-[1.388vw] lg:pt-[1.1111111111vw] lg:pb-[1.1111111111vw]  lg:flex-col  text-right px-[1.388vw]">
+            <div className="flex items-center lg:pt-0 lg:pb-0 lg:justify-end lg:mb-[3.555556vw]">
+              JYH
+              <svg className="mx-1 lg:mx-[0.20833333333333334vw] -mt-0.5 lg:mt-[-0.1388888888888889vw] w-3.5 lg:w-[0.9722222222222222vw] h-auto" width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.3806 12.398C10.1206 13.67 8.56058 14.306 6.70058 14.306C4.84058 14.306 3.27458 13.67 2.00258 12.398C0.742578 11.126 0.112578 9.53 0.112578 7.61C0.112578 5.69 0.742578 4.094 2.00258 2.822C3.27458 1.538 4.84058 0.895999 6.70058 0.895999C8.56058 0.895999 10.1206 1.538 11.3806 2.822C12.6526 4.094 13.2886 5.69 13.2886 7.61C13.2886 9.53 12.6526 11.126 11.3806 12.398ZM2.57858 11.912C3.67058 13.04 5.04458 13.604 6.70058 13.604C8.35658 13.604 9.73058 13.04 10.8226 11.912C11.9146 10.772 12.4606 9.338 12.4606 7.61C12.4606 5.882 11.9086 4.448 10.8046 3.308C9.71258 2.168 8.34458 1.598 6.70058 1.598C5.05658 1.598 3.68258 2.168 2.57858 3.308C1.48658 4.448 0.940578 5.882 0.940578 7.61C0.940578 9.338 1.48658 10.772 2.57858 11.912ZM6.75458 11.714C5.63858 11.714 4.72658 11.33 4.01858 10.562C3.31058 9.782 2.95658 8.792 2.95658 7.592C2.95658 6.38 3.29858 5.384 3.98258 4.604C4.67858 3.812 5.60858 3.416 6.77258 3.416C7.69658 3.416 8.44058 3.662 9.00458 4.154C9.58058 4.646 9.92258 5.276 10.0306 6.044H8.66258C8.57858 5.588 8.36858 5.228 8.03258 4.964C7.70858 4.7 7.28258 4.568 6.75458 4.568C5.98658 4.568 5.39258 4.85 4.97258 5.414C4.56458 5.978 4.36058 6.704 4.36058 7.592C4.36058 8.468 4.57658 9.182 5.00858 9.734C5.45258 10.286 6.03458 10.562 6.75458 10.562C7.33058 10.562 7.78658 10.4 8.12258 10.076C8.47058 9.752 8.67458 9.338 8.73458 8.834H10.0666C10.0426 9.65 9.74258 10.328 9.16658 10.868C8.56658 11.432 7.76258 11.714 6.75458 11.714Z" fill="#E6E8EA"></path>
+              </svg>
+              2025
+            </div>
+            <div className="flex flex-col-reverse lg:flex-row justify-between py-3.5 lg:py-0">
+              <nav className="flex justify-between lg:justify-end gap-[1.25vw]">
+                <a href="https://www.linkedin.com/company/atoll-digital/" target="_blank" >
+                  <span className="relative flex flex-col overflow-hidden">
+                    <span className="!opacity-100 flex nowrap items-center" >GIT</span>
+                    <span className="!opacity-100 flex nowrap items-center" >GIT</span>
+                  </span>
+                </a>
+                <a href="" target="_blank" className='overflow-hidden'>
+                  <span className="label_wrap">
+                    <span className="opacity-100" ></span>
+                    <span className="opacity-100"></span>
+                  </span>
+                </a>
+              </nav>
+
+              <div className="max-lg:hidden">
+                <ul className="flex gap-[1.25vw]">
+                  <li>
+                    <a href="#">
+                      <span className="relative flex flex-col overflow-hidden">
+                        <span className="!opacity-100 flex nowrap items-center" >PORTFOILO</span>
+                        <span className="!opacity-100 flex nowrap items-center" >PORTFOILO</span>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 

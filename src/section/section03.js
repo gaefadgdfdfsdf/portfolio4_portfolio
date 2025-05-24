3349b9b16b94b727982f8250c1875fcced583e29
Jo yun ha <ehdhbd688@naver.com>
2025-05-24 13:35:11 +0900

    d

--- src/section/section03.js
+++ src/section/section03.js
@@ -43,7 +43,7 @@ const Section03 = () => {
   return (
    <>
     <div ref={section03Ref} className='bg-[#091423] h-[500vh]'>
-        <div id="process_block" className='sticky top-0 '>
+        <div id="process_block" className='sticky top-[3.4722222222222223vw] '>
             <div className='lg:flex flex-col justify-between lg:h-[calc(100vh+1px-3.472vw)] lg:sticky lg:top-[var(3.4722222222222223vw)]'>
                 <section  className='pt-[25px] lg:pt-[2.1111111111111112vw]'>
                     <div className='w-max-full pl-[1.388vw] pr-[1.388vw]'>
